cf414cdd73140233df993415c2495ae8f5d4218c
KeeUi0 <77387339+KeeUi0@users.noreply.github.com>
2024-03-15 18:58:17 +0900

    first commit

---
+++ src/main/java/com/example/myhome_project/MyHomeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.myhome_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHomeProjectApplication.class, args);
+    }
+
+}
